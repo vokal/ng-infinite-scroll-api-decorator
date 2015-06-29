7ffddc71d8ec6f9a7d28a8426a23206ae99c9a61
Jarrett Widman <jarrett@24m2.com>
2015-06-29 16:19:27 -0500

    Initial clone to public

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ng-infinite-scroll-api-decorator
+
 Makes it easier to connect paged API responses to ngInfiniteScroll
+
+## Use
+
+To initialize, pass a URL with the token `{page}` which will be replaced as each page is loaded.
+
+```js
+$scope.list = new infiniteScroll( "/users?p={page}" );
+```
+
+The attributes shown here are attributes of ngInfiniteScroll, but the conditionals and callbacks are part of infiniteScrollDecorator.
+
+```html
+<tbody infinite-scroll="list.getNextPage()" infinite-scroll-distance="0"
+    infinite-scroll-disabled="list.busy || list.hasMore === false">
+    <tr data-ng-repeat="item in list.items">
+        ...
+    </tr>
+</tbody>
+```
+
+## Dependencies
+
+Currently, there is a dependency on toastr, this may be removed in a future version.
---
+++ source/infinite-scroll.js
@@ -0,0 +1,70 @@
+
+angular.module( "infiniteScrollDecorator", [] )
+.service( "infiniteScrollDecorator", [
+    function ()
+    {
+        "use strict";
+
+    	/*
+        * @param {string} url example: /users?p={page}
+    	*/
+        var infiniteScrollDecorator = function( url, API )
+        {
+            var scope = this;
+
+            scope.url = url;
+            scope.API = API;
+
+            scope.reload = function ()
+            {
+                scope.items = [];
+                scope.page = 1;
+                scope.busy = false;
+                scope.hasMore = true;
+                scope.getNextPage();
+            };
+
+            scope.getNextPage = function ()
+            {
+                if ( !scope.url || scope.busy || scope.hasMore === false )
+                {
+                    return;
+                }
+
+                scope.busy = true;
+
+                API.$get( url.replace( "{page}", scope.page ) )
+                    .then( function ( response )
+                    {
+                        var newItems = response.data.results;
+
+                        if ( newItems && newItems.length )
+                        {
+                            for ( var i = 0; i < newItems.length; i++ )
+                            {
+                                scope.items.push( newItems[ i ] );
+                            }
+                        }
+
+                        scope.hasMore = !!response.data.nextPage || !!response.data.next || false;
+                        scope.page++;
+                        scope.busy = false;
+
+                        if ( !scope.hasMore )
+                        {
+                            toastr.info( "All results loaded." );
+                        }
+
+                    },
+                    function()
+                    {
+                        toastr.error( "Error", "There was an Error" );
+                    } );
+            };
+
+            scope.reload();
+        };
+
+        return infiniteScrollDecorator;
+    }
+] );
